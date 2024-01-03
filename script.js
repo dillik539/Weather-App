1cab6a6f31f20996398ae217bc14bf67ef071ebe
Your Name <you@example.com>
2024-01-03 12:52:42 -0600

    Modified getData function to display data without decimal.

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const inputValue = document.querySelector('.inputValue')
 const submit = document.querySelector('.submit')
 const weatherInfoDiv = document.querySelector('.showWeatherInfo')
 
-//const city = inputValue.value
-
-
 
 const getData = () =>{
     const city = inputValue.value
@@ -17,15 +14,13 @@ const getData = () =>{
         {
             weatherInfoDiv.innerHTML = `<h2>${data.name}</h2>
             <h3>Weather: ${data.weather[0].description}</h3>
-            <h3>Current Temperature: ${data.main.temp}</h3>
-            <h3>Feels Like: ${data.main.feels_like}</h3>
-            <h3>Maximum: ${data.main.temp_max}</h3>
-            <h3>Minimum: ${data.main.temp_min}</h3>
-            <h3>Humidity: ${data.main.humidity}</h3>`
-            //console.log('Description: ' +data.weather[0].description)
+            <h3>Current Temperature: ${Math.floor(data.main.temp)}</h3>
+            <h3>Feels Like: ${Math.floor(data.main.feels_like)}</h3>
+            <h3>Maximum: ${Math.floor(data.main.temp_max)}</h3>
+            <h3>Minimum: ${Math.floor(data.main.temp_min)}</h3>
+            <h3>Humidity: ${Math.floor(data.main.humidity)}</h3>`
         })
         inputValue.value = ''
 }
 
 submit.onclick = () => getData()
-//submit.addEventListener('click', getData())
