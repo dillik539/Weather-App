32af7545b034bf598bd3541205b68d05d2db179a
Your Name <you@example.com>
2024-01-04 11:15:51 -0600

    Added Description HTML element that captures the weather description.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const apiKey = 'bc6c6d0406c5ecca7ea963f81b4b23bd'
 const submit = document.querySelector('.submit')
 
 
-const getData = (city) =>{
-    city = getCity()
+const getData = () =>{
+    const city = getCity()
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
     fetch(url)
     .then(response => response.json())
@@ -23,6 +23,7 @@ const getCity = () => {
 const displayWeatherData = (weatherData) =>{
     const cityName = document.getElementById('city-name')
     const weatherType = document.getElementById('weather-type')
+    const description = document.getElementById('desc')
     const currentTemp = document.getElementById('temp')
     const minTemp = document.getElementById('min-temp')
     const maxTemp = document.getElementById('max-temp')
@@ -30,6 +31,7 @@ const displayWeatherData = (weatherData) =>{
     const humidity = document.getElementById('humidity')
     cityName.innerText = `${weatherData.name}`
     weatherType.innerText = `${weatherData.weather[0].main}`
+    description.innerText = `${weatherData.weather[0].description}`
     currentTemp.innerText = `${Math.floor(weatherData.main.temp)}`
     minTemp.innerText = `${Math.floor(weatherData.main.temp_min)}`
     maxTemp.innerText = `${Math.floor(weatherData.main.temp_max)}`
